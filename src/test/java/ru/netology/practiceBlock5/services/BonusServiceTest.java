12513919f6138f675e7cba5ed0ea131dd97524e9
AnF <anfwork007@gmail.com>
2024-09-08 00:22:28 +0300

    Fix #1

--- src/test/java/ru/netology/practiceBlock5/services/BonusServiceTest.java
+++ src/test/java/ru/netology/practiceBlock5/services/BonusServiceTest.java
@@ -1,18 +1,19 @@
 package ru.netology.practiceBlock5.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class BonusServiceTest {
-    @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/BonusServiceData.csv")
+    public void test(String name, long expected, long amount, boolean registered) {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
-        long amount = 1000;
+        /*long amount = 1000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 30;*/
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -21,7 +22,7 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+    /*@Test
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
@@ -61,7 +62,7 @@ public class BonusServiceTest {
         long actual = service.calculate(amount, registered);
 
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 
 
 }
