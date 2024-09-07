9d7e850433844bc18ce79af8bc86b9b866b15fcc
AnF <anfwork007@gmail.com>
2024-09-07 23:29:21 +0300

    modified to ParameterizedTests

--- src/test/java/ru/netology/practiceBlock5/services/BonusServiceTest.java
+++ src/test/java/ru/netology/practiceBlock5/services/BonusServiceTest.java
@@ -1,18 +1,20 @@
 package ru.netology.practiceBlock5.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class BonusServiceTest {
-    @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/bonusServiceData.csv")
+    public void test1(String test, long expected, long amount, boolean registered) {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
+//        long amount = 1000;
+//        boolean registered = true;
+//        long expected = 30;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -21,14 +23,15 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateForRegisteredAndOverLimit() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/bonusServiceData.csv")
+    public void test2(String test, long expected, long amount, boolean registered) {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000;
+        /*long amount = 1_000_000;
         boolean registered = true;
-        long expected = 500;
+        long expected = 500;*/
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -37,26 +40,28 @@ public class BonusServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void notRegisteredOverLimit() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/bonusServiceData.csv")
+    public void test3(String test, long expected, long amount, boolean registered) {
         BonusService service = new BonusService();
 
-        long amount = 1000;
+        /*long amount = 1000;
         boolean registered = false;
-        long expected = 10;
+        long expected = 10;*/
 
         long actual = service.calculate(amount, registered);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public  void notRegisteredLimitNumberBorder() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/bonusServiceData.csv")
+    public void test4(String test, long expected, long amount, boolean registered) {
         BonusService service = new BonusService();
 
-        long amount = 500;
+        /*long amount = 500;
         boolean registered = false;
-        long expected = 5;
+        long expected = 5;*/
 
         long actual = service.calculate(amount, registered);
 
